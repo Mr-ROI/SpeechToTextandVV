65eaae2bb7b7c18607d0e1ce6c328a3cd9a7a0c1
Mr.ROI <mr.roi003@gmail.com>
2025-09-19 01:43:10 +0530

    info and testing

---
+++ README.md
@@ -0,0 +1,3 @@
+https://pyautogui.readthedocs.io/en/latest/quickstart.html
+https://docs.pytorch.org/audio/stable/tutorials/audio_io_tutorial.html
+https://realpython.com/playing-and-recording-sound-python/
--- testing.py
+++ testing.py
@@ -22,33 +22,33 @@ RATE = 44100
 RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "output.wav"
 
-p = pyaudio.PyAudio()
+# p = pyaudio.PyAudio()
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK)
+# stream = p.open(format=FORMAT,
+#                 channels=CHANNELS,
+#                 rate=RATE,
+#                 input=True,
+#                 frames_per_buffer=CHUNK)
 
-print("* recording")
+# print("* recording")
 
-frames = []
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data)
+# frames = []
+# for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
+#     data = stream.read(CHUNK)
+#     frames.append(data)
 
-print("* done recording")
+# print("* done recording")
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+# stream.stop_stream()
+# stream.close()
+# p.terminate()
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+# wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+# wf.setnchannels(CHANNELS)
+# wf.setsampwidth(p.get_sample_size(FORMAT))
+# wf.setframerate(RATE)
+# wf.writeframes(b''.join(frames))
+# wf.close()
 
 
 r = sr.Recognizer()
@@ -58,10 +58,16 @@ with sr.AudioFile(audio_path) as source:
 
     try:
         text = r.recognize_google(audio_data)
-        print("Transcribed text:")
         print(text)
-        speakTest(text)
+        if "hey roi" in text.lower():
+           speakTest("How can i help you?")
     except sr.UnknownValueError:
         print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print(f"Could not request results from Google Speech Recognition service; {e}")
+
+
+
+
+
+
